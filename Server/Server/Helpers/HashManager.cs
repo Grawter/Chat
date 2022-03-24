210a21fdd6a429cf547109d27fe76505a158555f
Кирилл Драгунов <terminator17067@mail.ru>
2022-03-24 19:50:28 +0300

    Изменение процесса хеширования

--- Server/Server/Helpers/HashManager.cs
+++ Server/Server/Helpers/HashManager.cs
@@ -6,21 +6,21 @@ namespace Server.Helpers
 {
     public class HashManager
     {
-        private static string sec_word = "NsoYaS206NmJHfFkUYLkQsfJHAfA8216ixv";
+        private static string salt2 = "NsoYaS206NmJHfFkUYLkQsfJHAfA8216ixv";
 
-        public static string GenerateHash(string password, byte[] salt)
+        public static string GenerateHash(string password, byte[] salt1)
         {
-            byte[] round1 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(sec_word), salt);
-            byte[] round2 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(password), round1);
+            byte[] round1 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(password), salt1);
+            byte[] round2 = SHA256Hash.GenerateSaltedHash(round1, Encoding.Unicode.GetBytes(salt2));
             string result = Convert.ToBase64String(round2);
 
             return result;
         }
 
-        public static bool Access(string password, byte[] salt, string hashpassword)
+        public static bool Access(string password, byte[] salt1, string hashpassword)
         {
-            byte[] round1 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(sec_word), salt);
-            byte[] round2 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(password), round1);
+            byte[] round1 = SHA256Hash.GenerateSaltedHash(Encoding.Unicode.GetBytes(password), salt1);
+            byte[] round2 = SHA256Hash.GenerateSaltedHash(round1, Encoding.Unicode.GetBytes(salt2));
             byte[] hashbyte = Convert.FromBase64String(hashpassword);
 
             return SHA256Hash.CompareByteArrays(hashbyte, round2);
